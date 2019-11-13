74e12b61831d298545b1c46acda56040d5a46bca
Hassan Shulli <hassan14x@hotmail.com>
2019-11-14 01:36:31 +0800

    observation form and list complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'birdObservations';
+  birdList = [];
+  checkoutForm;
+  state = 'ObservationList';
+
+  constructor(private formBuilder: FormBuilder) {
+    this.checkoutForm = this.formBuilder.group({
+      name: '',
+      notes: '',
+      rarity: ''
+    });
+  }
+
+
+  ngOnInit() {
+    this.birdList.push({
+      name: 'Yellow Bird',
+      notes: 'Healthy',
+      rarity: 'rare'
+    }, {
+      name: 'Grey Bird',
+      notes: 'Grey',
+      rarity: 'extremely rare'
+    });
+  }
+
+  changeState(s) {
+    this.state = s;
+  }
+
+  submitForm(value) {
+    this.birdList.push(value);
+    console.log('value : ', value);
+  }
 }
